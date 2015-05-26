e778643a3623701be9dcfaecf1b30886bb30d2f2
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-26 11:13:08 +0200

    ja

--- www/js/fototoevoegen.js
+++ www/js/fototoevoegen.js
@@ -6,14 +6,30 @@ jquery: true
 /*
 global CanvasCamera
 */
-$(document).ready(function () {
-	alert("deviceready");
-	console.log("jo");
+var pictureSource; // picture source
+var destinationType; // sets the format of returned value 
+
+// Wait for Cordova to connect with the device
+//
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready to be used!
+//
+function onDeviceReady() {
+	console.log(window.device);
+	console.log(window.plugins);
+	alert('device ready');
+	/*var objCanvas = document.getElementById("canvas");
+	window.plugin.CanvasCamera.initialize(objCanvas);
+	alert('canvas ready');*/
 	document.getElementById("add_photo").addEventListener("click", takePicture, false);
-});
+	//document.getElementById("takePicturePreview").addEventListener("click", onTakePicture, false);
+
+}
 
 function takePicture(e) {
-	alert("testgelukt");
 	navigator.camera.getPicture(onSuccess, onFail, {
 		quality: 50,
 		destinationType: navigator.camera.DestinationType.DATA_URL,
@@ -38,10 +54,12 @@ function onSuccess(imageData) {
 	var dataURL = canvas.toDataURL();
 	document.getElementById("defImg").setAttribute('crossOrigin', 'anonymous');
 	document.getElementById("defImg").src = dataURL;
+	//imageObj.src = "data:image/jpeg;base64," + imageData;
 }
 
 function onFail(message) {
 	setTimeout(function () {
+		//alert('Failed because: ' + message);
 	}, 0);
 
 }
